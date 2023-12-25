1720f87815e7cec2cf53ca4aef1409d8ee6e756c
Abdul rauf <141711142+Raufjatoi@users.noreply.github.com>
2023-12-25 20:57:13 +0500

    12/25/23         8:57 PM

---
+++ sub matrix.py
@@ -0,0 +1,9 @@
+mat1 = [[1, 2, 3], [4, 5, 6]]
+mat2 = [[7, 8, 9], [10, 11, 12]]
+mat3 = [[0, 0, 0], [0, 0, 0]]
+
+for i in range(len(mat1)):
+    for j in range(len(mat1[0])):
+        mat3[i][j] = mat1[i][j] / mat2[i][j]
+
+print(mat3)
